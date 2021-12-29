cc45c701700c0f895b3ec624e1430473c14cae3e
SumedhaKandepalli <93021001+SumedhaKandepalli@users.noreply.github.com>
2021-12-28 19:47:59 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,92 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+
+var ground;
+
+var top_wall;
+var ball;
+
+var btn2;
+
+var string;
+
+function setup() {
+  createCanvas(400,400);
+
+  engine = Engine.create();
+  world = engine.world;
+  
+   //var ball_options = {
+    //restitution: 0.5,
+ // }
+   
+  
+  btn2 = createImg('up.png');
+  btn2.position(20,30);
+  btn2.size(50,50);
+  btn2.mouseClicked(vForce);
+
+  ball = Bodies.circle(100,200,20,);
+  World.add(world,ball);
+
+  string = Matter.Constraint.create({
+    pointA: {x:200, y:20},
+    bodyB: ball,
+    pointB: {x:0, y:0},
+    length: 100,
+    stiffness: 0.04
+  })
+
+  World.add(world, string);
+  
+   
+  
+  
+
+  ground =new Ground(200,390,400,20);
+
+
+  //ball = Bodies.circle(100,200,20,);
+  //World.add(world,ball);
+  
+  
+  
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  
+  ellipse(ball.position.x,ball.position.y,20);
+
+
+  strokeWeight(10);
+  stroke(255);
+
+  line(string.pointA.x, string.pointA.y, ball.position.x, ball.position.y);
+  
+
+  ground.show();
+  
+  Engine.update(engine);
+}
+
+
+function vForce()
+{
+  Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0});
+}
+  
+
+
